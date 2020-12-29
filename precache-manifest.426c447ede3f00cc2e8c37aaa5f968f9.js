37430113405f4d75f6030acd7b8c7504b9bf0c9d
Saul Alvarez <saul.alvarez664@gmail.com>
2020-12-29 18:14:07 -0500

    deploy

---
+++ precache-manifest.426c447ede3f00cc2e8c37aaa5f968f9.js
@@ -0,0 +1,230 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf8352cf5224e03ca72319d400f139a3",
+    "url": "/404.html"
+  },
+  {
+    "revision": "edebadb119e071520c78",
+    "url": "/css/app.3e25d61b.css"
+  },
+  {
+    "revision": "b27656718011d749bca3",
+    "url": "/css/chunk-vendors.f29fc9cc.css"
+  },
+  {
+    "revision": "085b1dd8427dbeff10bd55410915a3f6",
+    "url": "/fonts/fa-brands-400.085b1dd8.ttf"
+  },
+  {
+    "revision": "0fabb6606be4c45acfeedd115d0caca4",
+    "url": "/fonts/fa-brands-400.0fabb660.eot"
+  },
+  {
+    "revision": "cac68c831145804808381a7032fdc7c2",
+    "url": "/fonts/fa-brands-400.cac68c83.woff2"
+  },
+  {
+    "revision": "dc0bd022735ed218df547742a1b2f172",
+    "url": "/fonts/fa-brands-400.dc0bd022.woff"
+  },
+  {
+    "revision": "05b53beb21e3ef13d28244545977152d",
+    "url": "/fonts/fa-regular-400.05b53beb.woff"
+  },
+  {
+    "revision": "1a78af4105d4d56e6c34f76dc70bf1bc",
+    "url": "/fonts/fa-regular-400.1a78af41.ttf"
+  },
+  {
+    "revision": "3a3398a6ef60fc64eacf45665958342e",
+    "url": "/fonts/fa-regular-400.3a3398a6.woff2"
+  },
+  {
+    "revision": "ad3a7c0d77e09602f4ab73db3660ffd8",
+    "url": "/fonts/fa-regular-400.ad3a7c0d.eot"
+  },
+  {
+    "revision": "781e85bb50c8e8301c30de56b31b1f04",
+    "url": "/fonts/fa-solid-900.781e85bb.ttf"
+  },
+  {
+    "revision": "89bd2e38475e441a5cd70f663f921d61",
+    "url": "/fonts/fa-solid-900.89bd2e38.eot"
+  },
+  {
+    "revision": "c500da19d776384ba69573ae6fe274e7",
+    "url": "/fonts/fa-solid-900.c500da19.woff2"
+  },
+  {
+    "revision": "ee09ad7553b8ad3d81150d609d5341a0",
+    "url": "/fonts/fa-solid-900.ee09ad75.woff"
+  },
+  {
+    "revision": "775a7fbc040fcdeb11c099d2602877df",
+    "url": "/img/covid-19.775a7fbc.svg"
+  },
+  {
+    "revision": "10f1314f990697211d758ca815730acb",
+    "url": "/img/difficulty-breathing.10f1314f.svg"
+  },
+  {
+    "revision": "ccfdb9dc442be0c629d331e94497428b",
+    "url": "/img/fa-brands-400.ccfdb9dc.svg"
+  },
+  {
+    "revision": "e75dfd904d366a2560c63c23cfc98ef8",
+    "url": "/img/fa-regular-400.e75dfd90.svg"
+  },
+  {
+    "revision": "03ba7cb710104df27f1c9c46d64bee4e",
+    "url": "/img/fa-solid-900.03ba7cb7.svg"
+  },
+  {
+    "revision": "11224669cb47a28579f1420f50d3d16d",
+    "url": "/img/fb-messenger.11224669.svg"
+  },
+  {
+    "revision": "3cf9a09bdd8367cf1df2ccfa93790afd",
+    "url": "/img/logo.3cf9a09b.png"
+  },
+  {
+    "revision": "c7c9b6857dcd909a60d5a727910a1280",
+    "url": "/img/news_icon.c7c9b685.svg"
+  },
+  {
+    "revision": "ed120311d405cc1b69693113f0c6c1af",
+    "url": "/img/search-engine.ed120311.svg"
+  },
+  {
+    "revision": "0dbc86fdca331bb512e3c49d0b5bb3b4",
+    "url": "/img/splash/apple-icon-120.jpg"
+  },
+  {
+    "revision": "99ff7d339d559e1a94ad5c0ca92aba82",
+    "url": "/img/splash/apple-icon-152.jpg"
+  },
+  {
+    "revision": "e5a54ae2cbc2ef4fd2dd810156f5979d",
+    "url": "/img/splash/apple-icon-167.jpg"
+  },
+  {
+    "revision": "0f4b345a95abeffe73056982332e4a8b",
+    "url": "/img/splash/apple-icon-180.jpg"
+  },
+  {
+    "revision": "ff4881a95b462db512954b1e6ce277b0",
+    "url": "/img/splash/apple-splash-dark-1080-1920.jpg"
+  },
+  {
+    "revision": "a8acc90cc3390dbe9dae34a798cef009",
+    "url": "/img/splash/apple-splash-dark-1125-2436.jpg"
+  },
+  {
+    "revision": "b632bd85925ac0e6c340f09cad6e94be",
+    "url": "/img/splash/apple-splash-dark-1136-640.jpg"
+  },
+  {
+    "revision": "790b078ff7f14f3053dfb099fdc78d18",
+    "url": "/img/splash/apple-splash-dark-1242-2688.jpg"
+  },
+  {
+    "revision": "d81b00111bd4c070a50752e939f45fbb",
+    "url": "/img/splash/apple-splash-dark-1334-750.jpg"
+  },
+  {
+    "revision": "ef3eee510431144fca078cceda7bc58b",
+    "url": "/img/splash/apple-splash-dark-1536-2048.jpg"
+  },
+  {
+    "revision": "1f1d48f55950f4c68992f0df7f78b7cd",
+    "url": "/img/splash/apple-splash-dark-1620-2160.jpg"
+  },
+  {
+    "revision": "4e3b6b26d8cb748837619ae6c8382806",
+    "url": "/img/splash/apple-splash-dark-1668-2224.jpg"
+  },
+  {
+    "revision": "f71e5709993919414b519d41ffbcf9d6",
+    "url": "/img/splash/apple-splash-dark-1668-2388.jpg"
+  },
+  {
+    "revision": "be4e5f445785cb9b3b5997c0a4db9063",
+    "url": "/img/splash/apple-splash-dark-1792-828.jpg"
+  },
+  {
+    "revision": "c7ce6b4596c73d4e5939fcc1919fbfa2",
+    "url": "/img/splash/apple-splash-dark-1920-1080.jpg"
+  },
+  {
+    "revision": "1f3302bc4089b90bd07d2ed410ea1b01",
+    "url": "/img/splash/apple-splash-dark-2048-1536.jpg"
+  },
+  {
+    "revision": "1442388c4944c12aed1040bfa4863851",
+    "url": "/img/splash/apple-splash-dark-2048-2732.jpg"
+  },
+  {
+    "revision": "806b332d1b396bb5ac0810eca0c4dc10",
+    "url": "/img/splash/apple-splash-dark-2160-1620.jpg"
+  },
+  {
+    "revision": "b8793aac11566b2574682b2391553d7b",
+    "url": "/img/splash/apple-splash-dark-2224-1668.jpg"
+  },
+  {
+    "revision": "0df66437f646be3ad2af131daee655dc",
+    "url": "/img/splash/apple-splash-dark-2388-1668.jpg"
+  },
+  {
+    "revision": "7f5f579dde4ecc67f3d08083f3f6a0ac",
+    "url": "/img/splash/apple-splash-dark-2436-1125.jpg"
+  },
+  {
+    "revision": "d929bf7523ade6f1397719ea98943907",
+    "url": "/img/splash/apple-splash-dark-2688-1242.jpg"
+  },
+  {
+    "revision": "7e2194b7256762da6658eac150c087b9",
+    "url": "/img/splash/apple-splash-dark-2732-2048.jpg"
+  },
+  {
+    "revision": "9dcf42c602be093226c0a92fac13fc52",
+    "url": "/img/splash/apple-splash-dark-640-1136.jpg"
+  },
+  {
+    "revision": "45ada56fc5169d09314e3bab5f011554",
+    "url": "/img/splash/apple-splash-dark-750-1334.jpg"
+  },
+  {
+    "revision": "0888545d9c85e954dcaa5cbeb32a8abb",
+    "url": "/img/splash/apple-splash-dark-828-1792.jpg"
+  },
+  {
+    "revision": "bd45e167a94afd5caed53b71efb9dd63",
+    "url": "/img/splash/manifest-icon-192.png"
+  },
+  {
+    "revision": "d8724321ea2ec97897e393783decb249",
+    "url": "/img/splash/manifest-icon-512.png"
+  },
+  {
+    "revision": "4b5e5d59686704778ab016ab5df6c043",
+    "url": "/index.html"
+  },
+  {
+    "revision": "edebadb119e071520c78",
+    "url": "/js/app.51670592.js"
+  },
+  {
+    "revision": "b27656718011d749bca3",
+    "url": "/js/chunk-vendors.d9fc3d8f.js"
+  },
+  {
+    "revision": "7873ffba5df8661553c65a6ea7c64471",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/robots.txt"
+  }
+]);
